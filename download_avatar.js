62eb0720d15a6e46536fd2fd6931af38c4de2ec6
Richard Hsieh <richardhsieh1@hotmail.com>
2017-02-14 01:59:23 +0000

    Added body and user-agent

--- download_avatar.js
+++ download_avatar.js
@@ -4,8 +4,15 @@ var GITHUB_TOKEN = "31dda3a1ee7b79cd1419a665e8589d63cbbbe825"
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb){
-  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' +repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
+  var requestURL =  { url: ('https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' +repoOwner + '/' + repoName + '/contributors'),
+                      headers: {
+                        'User-Agent': "GitHub Avatar Downloader - Student Project"
+                      }
+                    }
+  request(requestURL, function(err, response, body) {
+    if (err) throw err;
+  console.log(body);
+});
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
