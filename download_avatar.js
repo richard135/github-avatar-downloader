b4697406996841cc451f50642cd2ac5489a03b2e
Richard Hsieh <richardhsieh1@hotmail.com>
2017-02-14 03:33:56 +0000

    Linted the codes

--- download_avatar.js
+++ download_avatar.js
@@ -1,16 +1,13 @@
 var request = require('request');
 var fs = require('fs');
 var GITHUB_USER = "richard135";
-var GITHUB_TOKEN = "31dda3a1ee7b79cd1419a665e8589d63cbbbe825"
+var GITHUB_TOKEN = "31dda3a1ee7b79cd1419a665e8589d63cbbbe825";
 var myArgs = process.argv.slice(2);
 
 
 function getRepoContributors(repoOwner, repoName, cb){
-  var requestURL =  { url: ('https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' +repoOwner + '/' + repoName + '/contributors'),
-                      headers: {
-                        'User-Agent': "GitHub Avatar Downloader - Student Project"
-                      }
-                    }
+  var requestURL =  { url: ('https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors'),
+    headers: {'User-Agent': "GitHub Avatar Downloader - Student Project"}};
   request(requestURL, function(err, response, body) {
     var parsed = JSON.parse(body);
     cb(err, parsed);
@@ -18,26 +15,26 @@ function getRepoContributors(repoOwner, repoName, cb){
 }
 
 function downloadImageByURL(url, filePath){
-  request.get(url)               // Note 1
-       .on('error', function (err) {                                   // Note 2
+  request.get(url)
+       .on('error', function (err) {
          throw err;
        })
-       .on('response', function (response) {                           // Note 3
+       .on('response', function (response) {
          console.log('Response Status Code: ', response.statusCode);
        })
-       .pipe(fs.createWriteStream('avatar/'+filePath+'.jpg'));
+       .pipe(fs.createWriteStream('avatar/' + filePath + '.jpg'));
 }
 
 
 
 getRepoContributors(myArgs[0], myArgs[1], function(err, result) {
-    if (myArgs[0] == undefined)
+  if (myArgs[0] === undefined) {
     return console.log("Please enter the repo owner followed by repo name");
-
-    console.log("Errors:", err);
-    for (avatar of result) {
-      downloadImageByURL(avatar.avatar_url, avatar.login);
-    }
+  }
+  console.log("Errors:", err);
+  for (avatar of result) {
+    downloadImageByURL(avatar.avatar_url, avatar.login);
+  }
 });
 
 
