044d5cc8da67d28a45c5d5a877ce7108246c6184
Richard Hsieh <richardhsieh1@hotmail.com>
2017-02-14 00:56:28 +0000

    started the getRepoContributors function

--- download_avatar.js
+++ download_avatar.js
@@ -1,3 +1,13 @@
 var request = require('request');
+var GITHUB_USER = "richard135";
+var GITHUB_TOKEN = "31dda3a1ee7b79cd1419a665e8589d63cbbbe825"
+console.log('Welcome to the GitHub Avatar Downloader!');
 
-console.log('Welcome to the GitHub Avatar DOwnloader!');
+function getRepoContributors(repoOwner, repoName, cb){
+
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
