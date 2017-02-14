58299ed26f1dbf9d551305a34006568ba9cff35f
Richard Hsieh <richardhsieh1@hotmail.com>
2017-02-14 01:03:12 +0000

    Adding requested URL dynamically

--- download_avatar.js
+++ download_avatar.js
@@ -4,7 +4,8 @@ var GITHUB_TOKEN = "31dda3a1ee7b79cd1419a665e8589d63cbbbe825"
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb){
-
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' +repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
